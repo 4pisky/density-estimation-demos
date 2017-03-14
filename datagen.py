b7fecb01b4200190fe73a4cf5856908f5ec7911e
Tim Staley <git@timstaley.co.uk>
2017-03-14 18:29:57 +0000

    Figuring out xarray.
    Is the complexity worth it, vs a custom 'attrs'-style datastructure?

--- datagen.py
+++ datagen.py
@@ -1,6 +1,7 @@
 import numpy as np
-import scipy
-import xarray as xr
+import scipy.linalg
+import pandas as pd
+from copy import deepcopy
 
 import logging
 
@@ -12,18 +13,20 @@ def build_covariance_matrix(sigmas, correlations):
     Builds a covariance matrix from sigmas and correlations.
 
     Args:
-        sigmas (xarray.DataArray): Mapping of parameter name to std. dev.
+        sigmas (dict): Mapping of parameter name to std. dev.
         correlations (dict): Mapping of tuples to correlations, e.g.
             ``{(param1,param2):0.5}``. Default correlation for unspecified
             pairs is zero.
     Returns:
-        (DataFrame): Covariance matrix.
+        (pandas.DataFrame): Covariance matrix.
     """
-    cov = pd.DataFrame(index=sigmas.index,
-                       columns=sigmas.index,
+    sigmas = pd.Series(sigmas)
+    cov = pd.DataFrame(index=deepcopy(sigmas.index),
+                       columns=deepcopy(sigmas.index),
                        data=np.diag(sigmas ** 2),
                        dtype=np.float
                        )
+    cov.columns.name='ax2'
     for param_pair, pair_corr in correlations.items():
         p1, p2 = param_pair
         pair_cov = pair_corr * sigmas[p1] * sigmas[p2]
